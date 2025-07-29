cd4f07a38bc4faf52e15826b77b6141b9e377775
Anoushey-chandio <alayabaig999@gmail.com>
2025-07-29 13:59:59 -0700

    Fix hero section and build issue

---
+++ src/app/hero/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import React from "react";
+import Hero from "@/components/hero"; // Make sure Hero component is saved in components folder
+
+const Page = () => {
+  return <Hero />;
+};
+
+export default Page;
